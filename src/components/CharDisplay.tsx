b373131456c5f4deb5083b74bb99ee16c96fc8e1
graceoflives <tienngocbui2612@gmail.com>
2025-10-07 14:57:12 +0700

    add trial mark

--- src/components/CharDisplay.tsx
+++ src/components/CharDisplay.tsx
@@ -29,14 +29,28 @@ const CharDisplay = ({ char, isTrial }: Props) => {
           borderRadius: 1
         }}
       >
-        <img
-          src={`images/element/${detail.element}.png`}
-          style={{ position: 'absolute', width: 25, height: 25 }}
-        />
+        <img src={`images/element/${detail.element}.png`} style={{ position: 'absolute', width: 25, height: 25 }} />
         <img
           src={`images/character/${char?.name}.png`}
           style={{ width: '100%', height: '100%', objectFit: 'contain' }}
         />
+        {isTrial && (
+          <Box
+            sx={{
+              position: 'absolute',
+              right: 0,
+              top: 0,
+              backgroundColor: '#D36D84',
+              fontSize: '0.6rem',
+              fontWeight: 'bold',
+              padding: '4px 6px',
+              borderRadius: 1,
+              color: '#FFF'
+            }}
+          >
+            Trial
+          </Box>
+        )}
       </Box>
       {(isTrial || char?.level) && (
         <Typography variant='subtitle1' sx={{ fontSize: '0.75rem', width: '100%', textAlign: 'center' }}>
